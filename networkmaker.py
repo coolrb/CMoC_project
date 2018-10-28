98e24d68ae8d74997bdec59c6dca8722465d6769
Gabriel Brookman <gabriel@eduroam-169-45.dyn.carleton.edu>
2018-10-28 17:17:49 -0500

    changes for oct. 28th. started network.

--- networkmaker.py
+++ networkmaker.py
@@ -1,12 +1,38 @@
-from keras.models import Model
-from keras.layers import Input, LSTM
+import torch
+import torch.nn as nn
 
-oneHotDim = 37
+class SubjectGenerator(nn.Module):
+    """a class for our model"""
 
-inputLayer = Input(shape=(None,oneHotDim))
+    def __init__(self):
+        """creates a new model with our architecture etc."""
+        super(SubjectGenerator, self).__init__()
+        inputSize = 27 # all the letters and space
+        hidden1size = 100 # LSTM node count
+        hidden2size = 50 # fully connected node count
+        outputSize = 1 # output
 
-model = LSTM(50, activation="relu",return_sequences=True)(inputLayer)
-model = LSTM(50, activation="relu",return_sequences=True)(model)
-model = LSTM(1, activation="sigmoid",return_sequences=True)(model)
+        self.LSTMLayer = nn.LSTMCell(inputSize, hidden1size)
+        self.relu1 = nn.ReLU()
+        self.fullyConnectedLayer = nn.Linear(hidden1size, hidden2size)
+        self.relu2 = nn.ReLU()
+        self.outputLayer = nn.Linear(hidden2size, outputSize)
+        self.outputActivation = nn.Sigmoid()
 
-model = Model(inputLayer,model)
+    def forward(self, character, hidden, cell):
+        """a forward pass through the network, for predicting and training"""
+        newhid, newcell = self.LSTMLayer(character, (hidden,cell))
+        newhid = self.relu1(newhid)
+        newhid = self.fullyConnectedLayer(newhid)
+        newhid = self.relu2(newhid)
+        newhid = self.outputLayer(newhid)
+        newhid = self.outputActivation(newhid)
+        return newhid, newcell
+
+    def blank_cell_and_hidden(self):
+        """returns empty cells and hidden for the first pass"""
+        return torch.zeroes(1,self.hidden1size), torch.zeroes(1,self.hidden1size)
+
+    def save_model(self,destination):
+        """saves the model so we don't have to retrain"""
+        torch.save(self.state_dict(), destination)
