2933141ee68edb2f6cba18c7489df763ed9824b6
Gabriel Brookman <gabriel@eduroam-169-45.dyn.carleton.edu>
2018-11-07 16:49:59 -0600

    added dropout, fixed bugs

--- networkmaker.py
+++ networkmaker.py
@@ -10,16 +10,18 @@ class SubjectGenerator(nn.Module):
 
         self.learning_rate = .001
 
-        inputSize = 27 # all the letters and space
-        hidden1size = 100 # LSTM node count
-        hidden2size = 50 # fully connected node count
-        outputSize = 1 # output
+        self.inputSize = 27 # all the letters and space
+        self.hidden1size = 400 # LSTM node count
+        self.hidden2size = 200 # fully connected node count
+        self.outputSize = 1 # output
 
-        self.LSTMLayer = nn.LSTMCell(inputSize, hidden1size)
+        self.LSTMLayer = nn.LSTMCell(self.inputSize, self.hidden1size)
         self.relu1 = nn.ReLU()
-        self.fullyConnectedLayer = nn.Linear(hidden1size, hidden2size)
+        self.dropout1 = nn.Dropout()
+        self.fullyConnectedLayer = nn.Linear(self.hidden1size, self.hidden2size)
         self.relu2 = nn.ReLU()
-        self.outputLayer = nn.Linear(hidden2size, outputSize)
+        self.dropout2 = nn.Dropout()
+        self.outputLayer = nn.Linear(self.hidden2size, self.outputSize)
         self.outputActivation = nn.Sigmoid()
 
         self.blank_cell_and_hidden()
@@ -34,8 +36,12 @@ class SubjectGenerator(nn.Module):
         its loss on a per-word basis so we're doing it this way"""
         newhid, newcell = self.LSTMLayer(character, (self.hidden_carry,self.cell_carry))
         newhid = self.relu1(newhid)
+        newcell = self.relu1(newcell)
+        newhid = self.dropout1(newhid)
+        newcell = self.dropout1(newcell)
         newhid = self.fullyConnectedLayer(newhid)
         newhid = self.relu2(newhid)
+        newhid = self.dropout2(newcell)
         newhid = self.outputLayer(newhid)
         newhid = self.outputActivation(newhid)
         self.hidden_carry, self.cell_carry = newhid, newcell
@@ -46,9 +52,9 @@ class SubjectGenerator(nn.Module):
             forward_one_letter(self, character)
 
     def blank_cell_and_hidden(self):
-        """returns empty cells and hidden for the first pass"""
-        self.hidden_carry = torch.zeroes(1,self.hidden1size), torch.zeroes(1,self.hidden1size)
-        self.cell_carry = torch.zeroes(1,self.hidden1size), torch.zeroes(1,self.hidden1size)
+        """resets empty cells and hidden for the first pass"""
+        self.hidden_carry = torch.zeros(1,self.hidden1size)
+        self.cell_carry = torch.zeros(1,self.hidden1size)
 
     def save_model(self,destination):
         """saves the model so we don't have to retrain"""
@@ -57,9 +63,9 @@ class SubjectGenerator(nn.Module):
     def train_pattern(self, body, subject_key):
         '''
         trains the network on a single message based on the subject_key
-        subject line should be a list of 0's and 1's depending on if the corresponding word is in the subject line of the message
+        subject_key should be a list of 0's and 1's depending on if the corresponding word is in the subject line of the message
         note: MESSAGES are encoded the as a list of WORDS
-        WORDS are encoded as a list of CHARACTERS
+        WORDS are encoded as a list of CHARACTERS (ending with space)
         CHARACTERS are encoded as 27-dimensional 1-hot-vectors
         '''
         self.train()
@@ -68,7 +74,7 @@ class SubjectGenerator(nn.Module):
         self.blank_cell_and_hidden()
         for i in range(len(body)):
             self(body[i])
-            loss += self.criterion(self.hidden_carry(), subject_key[i])
+            loss += self.criterion(self.hidden_carry, subject_key[i])
         loss.backward()
         self.optimizer.step()
         return loss.data.numpy() / len(subject_key)
